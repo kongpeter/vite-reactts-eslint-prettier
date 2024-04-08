26cd1de9b45ff426b3e5b25da593d1e3ed98628d
kongpeter <xjtlu2014@outlook.com>
2024-04-08 18:18:35 +1000

    fix: lint fixed

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,9 @@
 // ./src/App.test.tsx
 // 这里文件后缀修改为 tsx，因为需要测试 dom
-import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+
+import { render, screen } from '@testing-library/react';
+
 import App from './App';
 
 describe('test', () => {
